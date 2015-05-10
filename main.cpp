e71390bd0b8c0f187200fa12800a7ad50b3ea6a6
KoJoVe <JoaoVicente@MacBook-Pro-de-Joao-2.local>
2015-05-10 18:56:53 -0300

    Todo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+/* TODO:
+
+-Criar struct do no com seguintes informações:
+  -Matriz para representar tabuleiro
+  -Lista de filhos
+
+-Criar struct de lista encadeada
+
+*/
+
 int main (void) {
 	printf("Hello World\n");
 }
+
